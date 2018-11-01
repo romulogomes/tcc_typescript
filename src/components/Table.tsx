b087d33d245f15fdb8abaee9d485eb5b8e981ca9
Romulo <romulogomes13@hotmail.com>
2018-11-01 10:41:54 -0300

    Add component Table

---
+++ src/components/Table.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+export interface DadosTabela{
+    id : number;
+}
+
+export interface TableProps {
+    dados : DadosTabela[];
+    selecionado : { id :number };
+    setSelecionado(selecionado : {id : number}) : void
+}
+
+export default class Table extends React.Component<TableProps, any> {
+  public render() {
+    const { dados } = this.props;
+    if(dados.length) {
+        const dado = dados[0];
+        return (
+            <div className="col-11">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            { dado ? Object.keys(dados[0]).map(chave =>  <th className="titulo_table" key={chave}>{[chave]}</th> ) : false }
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { dados.map(dado => 
+                            <tr key={dado.id} className={`${dado.id === this.props.selecionado.id ? 'table-primary' : ''}`} onClick={() => this.props.setSelecionado(dado)}>
+                                { Object.keys(dado).map(chave =>
+                                    <td key={chave}>{dado[chave]}</td>    
+                                )}
+                            </tr>
+                        )}
+                    </tbody>
+                </table>            
+            </div>
+        );
+    }
+    else return ( "" ) 
+}
+}
