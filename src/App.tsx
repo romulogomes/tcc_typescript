5ea4963614f8b2e139084c109b78bdb70b2eb106
Romulo <romulogomes13@hotmail.com>
2018-11-01 08:50:04 -0300

    add List Orientadores

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header';
+import ListOrientadores from './components/orientadores/List';
 
 class App extends Component {
   render() {
---
+++ src/components/orientadores/List.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface ListOrientadoresProps {
+}
+
+export default class ListOrientadores extends React.Component<ListOrientadoresProps, any> {
+  public render() {
+    return (
+      <div>
+        List Orientadores
+      </div>
+    );
+  }
+}
