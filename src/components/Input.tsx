3d268ec20763d6065354e3d10cff2a676750b6d4
Romulo <romulogomes13@hotmail.com>
2018-11-01 13:41:25 -0300

    add componente inpu

---
+++ src/components/Input.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface InputProps {
+}
+
+export default class Input extends React.Component<InputProps, any> {
+  public render() {
+    return (
+      <div>
+        Input
+      </div>
+    );
+  }
+}
