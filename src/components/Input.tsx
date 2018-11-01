b4054de856a9cb3652d3ebb6fb696ab553024a63
Romulo <romulogomes13@hotmail.com>
2018-11-01 16:16:34 -0300

    refactory inputtext

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,14 +4,13 @@ import { Field } from 'react-final-form'
 export interface InputProps {
     label: string;
     name: string;
-
 }
 
 export default class InputText extends React.Component<InputProps, any> {
   public render() {
     return (
         <div className="form-group">
-        <label htmlFor="name">{this.props.label}</label>
+            <label htmlFor="name">{this.props.label}</label>
             <Field name={this.props.name} component="input" type="text" className="form-control" />
         </div>
     );
